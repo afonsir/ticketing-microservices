893f60fcc36447d8170962853987ab1dedb4ea56
AfonSir <afonsir17@gmail.com>
2021-05-06 19:39:49 -0300

    feat: add sign in router

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 
 import { currentUserRouter } from './routes/current-user'
+import { signinRouter } from './routes/signin'
 
 const app = express()
 
 app.use(express.json())
 
 app.use(currentUserRouter)
+app.use(signinRouter)
 
 app.listen(3000, () => {
   console.log('Listening on port 3000!')

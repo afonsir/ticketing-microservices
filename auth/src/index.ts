036e884e16646eddf9b809d8b30d0961deeb55e6
AfonSir <afonsir17@gmail.com>
2021-05-06 19:40:18 -0300

    feat: add sign out router

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
+import { signoutRouter } from './routes/signout'
 
 const app = express()
 
@@ -9,6 +10,7 @@ app.use(express.json())
 
 app.use(currentUserRouter)
 app.use(signinRouter)
+app.use(signoutRouter)
 
 app.listen(3000, () => {
   console.log('Listening on port 3000!')

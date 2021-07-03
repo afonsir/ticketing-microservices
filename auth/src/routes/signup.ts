31225099a6a50dc89b474fa2b69669912aad755f
AfonSir <afonsir17@gmail.com>
2021-07-03 15:49:14 -0300

    feat: add validation to signup

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,29 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import { body, validationResult } from 'express-validator'
 
 const router = express.Router()
 
-router.post('/api/users/signup', (request, response) => {
-  response.send('Hi there again!')
+router.post(
+  '/api/users/signup',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 6, max: 20 })
+      .withMessage('Password must be between 6 and 20 characters')
+  ],
+  (request: Request, response: Response) => {
+    const errors = validationResult(request)
+
+    if (!errors.isEmpty()) {
+      return response.status(400).send(errors.array())
+    }
+
+    const { email, password } = request.body
+
+    response.send({})
 })
 
 export { router as signupRouter }

0b32f487c11f341d7413f37c85c558c71628ff6c
AfonSir <afonsir17@gmail.com>
2021-12-18 22:24:18 -0300

    feat: add password hashing to user model

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose, { Schema, Model, Document } from 'mongoose'
+import { Password } from '../services/password'
 
 // An interface that describes the properties
 // that are required to create a new User
@@ -23,6 +24,15 @@ interface UserDocument extends Document {
 const userSchema = new Schema({
   email:    { type: String, required: true },
   password: { type: String, required: true }
+})
+
+userSchema.pre('save', async function(done) {
+  if (this.isModified('password')) {
+    const hashedPassword = await Password.toHash(this.get('password'))
+
+    this.set('password', hashedPassword)
+  }
+  done()
 })
 
 userSchema.statics.build = (attrs: UserAttrs) => {

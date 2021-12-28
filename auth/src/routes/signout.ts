4faed1fcd4e9065624e1d681b3d0e28cb8790c81
AfonSir <afonsir17@gmail.com>
2021-12-28 20:18:53 -0300

    feat: add signout logic to controller

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,8 @@ import express from 'express'
 const router = express.Router()
 
 router.post('/api/users/signout', (request, response) => {
-  response.send('Hi there again!')
+  request.session = null
+  response.send({})
 })
 
 export { router as signoutRouter }

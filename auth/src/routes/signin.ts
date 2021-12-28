96e7c549d3eea65eb0fc782a6734a631270ccda8
AfonSir <afonsir17@gmail.com>
2021-12-28 19:29:28 -0300

    refactor: add validation to signin controller

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,23 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import { body } from 'express-validator'
+
+import { validateRequest } from '../middlewares/validate-request'
 
 const router = express.Router()
 
-router.post('/api/users/signin', (request, response) => {
-  response.send('Hi there again!')
-})
+router.post('/api/users/signin',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must supply a password')
+  ],
+  validateRequest,
+  (request: Request, response: Response) => {
+  }
+)
 
 export { router as signinRouter }

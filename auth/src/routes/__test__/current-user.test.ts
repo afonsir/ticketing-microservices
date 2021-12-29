bdb7867b591982837df1587ad369130283eb909c
AfonSir <afonsir17@gmail.com>
2021-12-29 20:23:56 -0300

    test: ensure currentUser controller responds with null if not authenticated

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -11,3 +11,11 @@ it('responds with details about the current user', async () => {
 
   expect(response.body.currentUser.email).toEqual('any_email@mail.com')
 })
+
+it('responds with null if not authenticated', async () => {
+  const response = await request(app)
+    .get('/api/users/current_user')
+    .expect(200)
+
+  expect(response.body.currentUser).toBeNull()
+})

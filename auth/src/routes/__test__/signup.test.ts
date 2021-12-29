db30146148bfe858440951f1f0124514becddcd8
AfonSir <afonsir17@gmail.com>
2021-12-29 19:47:38 -0300

    test: ensure signUp controller sets a cookie after a successful request

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -64,3 +64,15 @@ it('disallows duplicated emails', async () => {
     })
     .expect(400)
 })
+
+it('sets a cookie after successful signup', async () => {
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    .expect(201)
+
+  expect(response.get('Set-Cookie')).toBeDefined()
+})

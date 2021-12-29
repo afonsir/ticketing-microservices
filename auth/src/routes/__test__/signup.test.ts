9e129479f645be1a2d6b0d07b95212f60fe2cced
AfonSir <afonsir17@gmail.com>
2021-12-29 19:40:27 -0300

    test: ensure signUp controller returns 400 with invalid email or password

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,39 @@ it('returns a 201 on successful signup', () => {
     })
     .expect(201)
 })
+
+it('returns a 400 with an invalid email', () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'invalid_email',
+      password: 'any_password'
+    })
+    .expect(400)
+})
+
+it('returns a 400 with an invalid password', () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'any_email@mail.com',
+      password: 'any'
+    })
+    .expect(400)
+})
+
+it('returns a 400 with missing email and password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'any_email@mail.com',
+    })
+    .expect(400)
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      password: 'any_password'
+    })
+    .expect(400)
+})

4087abfc2c7bbe72e5dd359ef5494d0cba35c7f8
AfonSir <afonsir17@gmail.com>
2022-01-04 19:47:51 -0300

    refactor: add error handling to signup page

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -5,15 +5,18 @@ import { useState } from 'react'
 const SignUp = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [errors, setErrors] = useState([])
 
   const onSubmit = async (event) => {
     event.preventDefault()
 
-    const response = await axios.post('/api/users/signup', {
-      email, password
-    })
-
-    console.log(response.data)
+    try {
+      const response = await axios.post('/api/users/signup', {
+        email, password
+      })
+    } catch(err) {
+      setErrors(err.response.data.errors)
+    }
   }
 
   return (
@@ -39,6 +42,18 @@ const SignUp = () => {
           className="form-control"
         />
       </div>
+
+      {errors.length > 0 && (
+        <div className="alert alert-danger">
+          <h4>Ooops....</h4>
+
+          <ul className="my-0">
+            {errors.map(err => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
 
       <button type="submit" className="btn btn-primary">Sign Up</button>
     </form>

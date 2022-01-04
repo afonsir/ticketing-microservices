2c907d6d43b7388c3a4ae0ada076fa21b0a7fec6
AfonSir <afonsir17@gmail.com>
2022-01-04 20:07:39 -0300

    refactor: move request to custom hook

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,22 +1,23 @@
-import axios from 'axios'
-
 import { useState } from 'react'
+
+import useRequest from '../../hooks/use-request'
 
 const SignUp = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [errors, setErrors] = useState([])
+
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: {
+      email, password
+    }
+  })
 
   const onSubmit = async (event) => {
     event.preventDefault()
 
-    try {
-      const response = await axios.post('/api/users/signup', {
-        email, password
-      })
-    } catch(err) {
-      setErrors(err.response.data.errors)
-    }
+    doRequest()
   }
 
   return (
@@ -43,17 +44,7 @@ const SignUp = () => {
         />
       </div>
 
-      {errors.length > 0 && (
-        <div className="alert alert-danger">
-          <h4>Ooops....</h4>
-
-          <ul className="my-0">
-            {errors.map(err => (
-              <li key={err.message}>{err.message}</li>
-            ))}
-          </ul>
-        </div>
-      )}
+      {errors}
 
       <button type="submit" className="btn btn-primary">Sign Up</button>
     </form>

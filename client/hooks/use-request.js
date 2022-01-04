a08c28648a511d3ee39fe985a39c3d7267dccc91
AfonSir <afonsir17@gmail.com>
2022-01-04 20:13:44 -0300

    refactor: add onSuccess callback to useRequest hook

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 import { useState } from 'react'
 
-const useRequest = ({ url, method, body }) => {
+const useRequest = ({ url, method, body, onSuccess }) => {
   const [errors, setErrors] = useState(null)
 
   const doRequest = async () => {
@@ -10,6 +10,10 @@ const useRequest = ({ url, method, body }) => {
       setErrors(null)
 
       const response = await axios[method](url, body)
+
+      if (onSuccess) {
+        onSuccess(response.data)
+      }
 
       return response.data
     } catch(err) {
--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,3 +1,5 @@
+import Router from 'next/router'
+
 import { useState } from 'react'
 
 import useRequest from '../../hooks/use-request'
@@ -11,13 +13,14 @@ const SignUp = () => {
     method: 'post',
     body: {
       email, password
-    }
+    },
+    onSuccess: () => Router.push('/')
   })
 
   const onSubmit = async (event) => {
     event.preventDefault()
 
-    doRequest()
+    await doRequest()
   }
 
   return (

a41a2259f4050156c388f45bede1aa89b00ecebf
AfonSir <afonsir17@gmail.com>
2022-01-04 21:51:56 -0300

    feat: add initial props to landing page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,26 @@
-const Landing = () => {
+import axios from 'axios'
+
+const LandingPage = ({ currentUser }) => {
+  console.log('currentUser', currentUser)
+
   return <h1>Landing Page</h1>
 }
 
-export default Landing
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    const { data } = await axios.get(
+      'http://local-ingress-srv/api/users/current_user',
+      {
+        headers: req.headers
+      }
+    )
+
+    return data
+  } else {
+    const { data } = await axios.get('/api/users/current_user')
+
+    return data
+  }
+}
+
+export default LandingPage

b2d098b33a85034506fc37553f8340f3689ea9f6
AfonSir <afonsir17@gmail.com>
2022-01-11 22:34:01 -0300

    feat: add current_user to app component

--- client/pages/index.js
+++ client/pages/index.js
@@ -9,7 +9,7 @@ const LandingPage = ({ currentUser }) => {
 }
 
 LandingPage.getInitialProps = async (context) => {
-  const client= buildClient(context)
+  const client = buildClient(context)
   const { data } = await client.get('/api/users/current_user')
 
   return data

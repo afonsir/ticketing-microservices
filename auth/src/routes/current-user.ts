0a17019fc197e002eb1b5fb09a4bc5e5128fe376
AfonSir <afonsir17@gmail.com>
2022-01-24 22:29:24 -0300

    refactor: use common package

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import { currentUser } from '../middlewares/current-user'
+import { currentUser } from '@afonsir-ticketing/common'
 
 const router = express.Router()
 
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -3,8 +3,7 @@ import { body } from 'express-validator'
 
 import jwt from 'jsonwebtoken'
 
-import { validateRequest } from '../middlewares/validate-request'
-import { BadRequestError } from '../errors/bad-request-error'
+import { validateRequest, BadRequestError } from '@afonsir-ticketing/common'
 
 import { Password } from '../services/password'
 import { User } from '../models/user'
